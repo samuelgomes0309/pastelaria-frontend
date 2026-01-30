ee3cbbda01ac3bb26a73621675dca45d73ec3a39
samuelgomes0309 <samuelgs0309@gmail.com>
2026-01-29 21:49:35 -0300

    Create forms login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,9 @@
+import LoginForm from "@/components/loginforms/login/loginForm";
+
 export default function Login() {
-	return <div>Login Page</div>;
+	return (
+		<div className="bg-app-background flex min-h-screen w-full items-center justify-center px-4 py-10">
+			<LoginForm />
+		</div>
+	);
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,3 +1,9 @@
+import RegisterForm from "@/components/loginforms/register/registerForm";
+
 export default function Register() {
-	return <div>Register Page</div>;
+	return (
+		<div className="bg-app-background min-h-screen w-full justify-center items-center flex py-10 px-4">
+			<RegisterForm />
+		</div>
+	);
 }
