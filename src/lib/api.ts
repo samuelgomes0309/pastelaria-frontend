9b8a8dcf5ecaa25fc05a093881dc7f5af167467a
samuelgomes0309 <samuelgs0309@gmail.com>
2026-02-14 14:39:18 -0300

    Update api confi

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -11,16 +11,5 @@ export const setupApi = async () => {
 			Authorization: token ? `Bearer ${token}` : "",
 		},
 	});
-	api.interceptors.response.use(
-		(response) => response,
-		(error) => {
-			if (error.response) {
-				if (error.response.status === 401) {
-					window.location.href = "/login";
-				}
-			}
-			return Promise.reject(error);
-		}
-	);
 	return api;
 };
